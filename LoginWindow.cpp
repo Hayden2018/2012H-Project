1142c936893edad0568dbf28379a52d1229793ea
victorlwt <41510514+victorlwt@users.noreply.github.com>
2018-12-02 21:52:06 +0800

    Update LoginWindow.cpp

--- LoginWindow.cpp
+++ LoginWindow.cpp
@@ -4,8 +4,8 @@
 LoginWindow::LoginWindow(QWidget *parent) :
     QDialog(parent),
     ui(new Ui::LoginWindow),
-    USERNAME("comp2012h"),
-    PASSWORD("comp2012hdesmond")
+    USERNAME("comp2012h"),          //setting username
+    PASSWORD("comp2012hdesmond")    //setting password
 {
     ui->setupUi(this);
 }
@@ -17,13 +17,13 @@ LoginWindow::~LoginWindow()
 
 void LoginWindow::on_buttonBox_accepted()
 {
-    QString usr = ui->m_userNameLineEdit->text();
-    QString pwd = ui->m_passwordLineEdit->text();
+    QString usr = ui->m_userNameLineEdit->text();       //Get username from the corresponding line edit
+    QString pwd = ui->m_passwordLineEdit->text();       //Get password from the corresponding line edit
 
-    if(usr == USERNAME  && pwd == PASSWORD){
-        emit loginSuccess();
+    if(usr == USERNAME  && pwd == PASSWORD){        //Check if login information is correct
+        emit loginSuccess();                        //If both are correct, emit success signal
     }
     else{
-        emit loginFail();
+        emit loginFail();                           //If either one is wrong, emit fail signal
     }
 }
