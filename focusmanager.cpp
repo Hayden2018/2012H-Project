30b0ef26aa5b2caa5d171b36763c917d3704dec8
Hayden2018 <39459225+Hayden2018@users.noreply.github.com>
2018-12-02 22:41:22 +0800

    Add comment

--- focusmanager.cpp
+++ focusmanager.cpp
@@ -7,41 +7,42 @@
 #include <algorithm>
 #include <iostream>
 
+//Performs checking when the user tries to visit websites via the URL bar
 void FocusManager::handleTypeAccess(QLineEdit* edit){
 
-    QString s = edit->text();
-    if(!s.contains("https://"))
+    QString s = edit->text(); // Get URL text
+    if(!s.contains("https://"))  // Correct URL format
         s = "https://" + s;
     QUrl url = QUrl(s);
 
     if(isWhitelisted(url)){
-        emit pass();
+        emit pass();  // If it is in white list emit pass signal
     }
     else{
-        QMessageBox::information(nullptr, "Calm Down", "You are about to get distracted, Stay Focus!");
+        QMessageBox::information(nullptr, "Calm Down", "You are about to get distracted, Stay Focus!"); // Warning otherwise
     }
 
 }
 
+// Performs checking when the user clicks on a link
 void FocusManager::handleClickAccess(QLineEdit* edit, WebView* tab){
 
-    QUrl url = tab->url();
-    if (url.toEncoded() == "") return;
+    QUrl url = tab->url(); // Get link URL
+    if (url.toEncoded() == "") return; // Prevent invalid message during construction of tab
 
     if(isWhitelisted(url)){
-        edit->setText(url.toDisplayString());
-        tab->is_new = false;
+        edit->setText(url.toDisplayString()); // If URL is in whitelist set URL bar text
+        tab->is_new = false; // Set to false because an URL has already been loaded
     }
     else{
         QMessageBox::information(nullptr, "Calm Down", "You are about to get distracted, Stay Focus!");
         if(tab->is_new)
-            tab->deleteLater();
-        tab->back();
+            tab->deleteLater(); // If a new tab is not in whitelist close it
+        tab->back(); // If a old tab leave the whitelist go back to previous page
     }
 }
 
-
-//Need to Implement Read File
+//Constructor
 FocusManager::FocusManager() :
     QObject(),
     recordDirectory(QStandardPaths::writableLocation(QStandardPaths::DataLocation) + "/comp2012h_project"),
@@ -52,54 +53,61 @@ FocusManager::FocusManager() :
     if (!QDir(recordDirectory).exists())
             QDir().mkpath(recordDirectory);
 
+    //Prepare to read file
     QFile file(recordPath);
     file.open(QIODevice::ReadOnly | QIODevice::Text);
-    QTextStream in(&file);
+    QTextStream in(&file); 
 
     while (!in.atEnd())
-        whitelist.insert(in.readLine());
+        whitelist.insert(in.readLine()); // Read and insert to AVL
 
-    whitelist.insert(QUrl("https://www.ust.hk").host());
-    whitelist.insert(QUrl("https://www.google.com").host());
+    whitelist.insert(QUrl("https://www.ust.hk").host()); // Default whitelist
+    whitelist.insert(QUrl("https://www.google.com").host()); // Default whitelist
 
     whitelist.remove("");
 
     file.close();
 }
 
-
+//Destructor
 FocusManager::~FocusManager()
 {
     QFile file(recordPath);
-    file.remove();
+    file.remove(); //Clean existing file
 
+    //Prepare to write file
     file.open(QIODevice::WriteOnly | QIODevice::Text | QIODevice::Truncate);
     QTextStream out(&file);
 
+    //Wirte all URLs in whitelist to file
     whitelist.in_order_action([&](const QString& s){out << s << endl;});
 }
 
 
+// Check whether URL is in whitelist
 bool FocusManager::isWhitelisted(const QUrl& url) const
 {
-    if(!onFocus) return true;
+    if(!onFocus) return true; // Alwats true if focus mode is off
     return whitelist.contains(url.host());
 }
 
+// Add an URL to the whitelist
 void FocusManager::addToWhitelist(const QUrl& url)
 {
-    if (url.toEncoded() == "")
+    if (url.toEncoded() == "") // Prevent empty URL input
         return;
 
     whitelist.insert(url.host());
-//    whitelist.remove("");
 }
 
+//Delete an URL from the whitelist
 void FocusManager::deleteFromWhitelist(const QUrl& url)
 {
     return whitelist.remove(url.host());
 }
 
+// Other users can communicate with THE focus manager through fm(). 
+// This is to prevent multiple instantiation of focusmanger i.e. enforcing singleton. 
 FocusManager& fm()
 {
     static FocusManager fm;
